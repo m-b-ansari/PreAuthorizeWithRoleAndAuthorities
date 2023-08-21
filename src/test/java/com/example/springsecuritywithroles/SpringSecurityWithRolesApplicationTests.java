908ab76daaec257a1ccc332a725949281c35695e
DC\mansari <divinehemlock@gmail.com>
2023-08-21 12:53:25 +0330

    init

---
+++ src/test/java/com/example/springsecuritywithroles/SpringSecurityWithRolesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritywithroles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithRolesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
