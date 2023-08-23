4fa06051b97ef3670166edee1fb74d50d48a0e62
DC\mansari <mansariwork2023@gmail.com>
2023-08-23 17:14:19 +0330

    implemented delegating password encoder

--- src/main/java/com/example/springsecuritywithroles/CommandLineRunner.java
+++ src/main/java/com/example/springsecuritywithroles/CommandLineRunner.java
@@ -1,42 +1,42 @@
-//package com.example.springsecuritywithroles;
-//
-//import com.example.springsecuritywithroles.enums.Role;
-//import com.example.springsecuritywithroles.model.User;
-//import com.example.springsecuritywithroles.user.UserRepository;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.*;
-//
-//@Component
-//public class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
-//    private final UserRepository repository;
-//
-//    public CommandLineRunner(UserRepository repository) {
-//        this.repository = repository;
-//    }
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        User writer = new User();
-//        writer.setUsername("writer");
-//        writer.setPassword("1234");
-//        writer.setRole(Role.WRITE);
-//        User reader = new User();
-//        reader.setUsername("reader");
-//        reader.setPassword("1234");
-//        reader.setRole(Role.READ);
-//        User patcher = new User();
-//        patcher.setUsername("patcher");
-//        patcher.setPassword("1234");
-//        patcher.setRole(Role.PATCH);
-//        User deleter = new User();
-//        deleter.setUsername("deleter");
-//        deleter.setPassword("1234");
-//        deleter.setRole(Role.DELETE);
-//        User admin = new User();
-//        admin.setUsername("admin");
-//        admin.setPassword("1234");
-//        admin.setRole(Role.ADMIN);
-//        repository.saveAll(Arrays.asList(admin, deleter, patcher, reader, writer));
-//    }
-//}
+package com.example.springsecuritywithroles;
+
+import com.example.springsecuritywithroles.enums.Role;
+import com.example.springsecuritywithroles.model.User;
+import com.example.springsecuritywithroles.user.UserRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+public class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
+    private final UserRepository repository;
+
+    public CommandLineRunner(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        User writer = new User();
+        writer.setUsername("writer");
+        writer.setPassword("{noop}1234");
+        writer.setRole(Role.WRITE);
+        User reader = new User();
+        reader.setUsername("reader");
+        reader.setPassword("{bcrypt}$2a$12$INxoGDBDZp2vlEz1GwTOyuJGL74jx010FXpF9p2shRpaX/PNdt.5i");
+        reader.setRole(Role.READ);
+        User patcher = new User();
+        patcher.setUsername("patcher");
+        patcher.setPassword("{bcrypt}$2a$12$INxoGDBDZp2vlEz1GwTOyuJGL74jx010FXpF9p2shRpaX/PNdt.5i");
+        patcher.setRole(Role.PATCH);
+        User deleter = new User();
+        deleter.setUsername("deleter");
+        deleter.setPassword("{bcrypt}$2a$12$INxoGDBDZp2vlEz1GwTOyuJGL74jx010FXpF9p2shRpaX/PNdt.5i");
+        deleter.setRole(Role.DELETE);
+        User admin = new User();
+        admin.setUsername("admin");
+        admin.setPassword("{bcrypt}$2a$12$INxoGDBDZp2vlEz1GwTOyuJGL74jx010FXpF9p2shRpaX/PNdt.5i");
+        admin.setRole(Role.ADMIN);
+        repository.saveAll(Arrays.asList(admin, deleter, patcher, reader, writer));
+    }
+}
--- src/main/java/com/example/springsecuritywithroles/SpringSecurityWithRolesApplication.java
+++ src/main/java/com/example/springsecuritywithroles/SpringSecurityWithRolesApplication.java
@@ -3,8 +3,13 @@ package com.example.springsecuritywithroles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class SpringSecurityWithRolesApplication {
@@ -15,6 +20,9 @@ public class SpringSecurityWithRolesApplication {
 
     @Bean
     public PasswordEncoder getEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        Map<String, PasswordEncoder> encoderMap = new HashMap<>();
+        encoderMap.put("noop", NoOpPasswordEncoder.getInstance());
+        encoderMap.put("bcrypt", new BCryptPasswordEncoder());
+        return new DelegatingPasswordEncoder("noop", encoderMap);
     }
 }
